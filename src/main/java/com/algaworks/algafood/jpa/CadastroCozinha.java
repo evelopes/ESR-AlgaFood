ea7c70c92bdc0f8fbea2c0320ad475aea30ad45c
evelopes <evelinheks@gmail.com>
2023-09-01 17:19:17 -0300

    3.8. Consultando objetos do banco de dados part.1

---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.model.domain.Cozinha;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+    @PersistenceContext
+    private EntityManager manager;
+    public List<Cozinha> listar(){
+        TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
+        return query.getResultList();
+    };
+}
